f5756d711f9f836ff638e44d64a1230458c51567
xiaoxudong [肖旭东] <xiaoxudong@xiaomi.com>
2018-05-26 16:44:42 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'accounting'
+  }
+</script>
+
+<style>
+  @import "./assets/css/common.css";
+  /* CSS */
+</style>
