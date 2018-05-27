b02db89da158d8dc11870b54d00210770e9f57e2
xiaoxudong [肖旭东] <xiaoxudong@xiaomi.com>
2018-05-27 22:49:54 +0800

    v1.1.1

--- src/renderer/view/index.vue
+++ src/renderer/view/index.vue
@@ -3,19 +3,35 @@
     <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <div class="doc">
       <div class="title">Getting Started</div>
-      <button @click="calc">开始计算</button><br><br>
+      <input @change="uploadfile($event)" type="file" value="上传文件" />
+      <button @click="calc">开始计算</button>
+      <button @click="action">{{isShowResult ? '收起数据' : '展开数据'}}</button>
+      <button @click="clear">清除数据</button>
+      <br><br>
+      <input v-model="companyId" type="text" placeholder="请输入公司">
+      <input v-model="year" type="text" placeholder="请输入年份">
+      <span class="tips">*检索前请先计算</span>
+      <br><br/>
+      <button @click="search">开始检索</button>
+      <button @click="download">下载excel</button>
+      <span v-if="result">检索结果:{{result}}</span>
+      <br><br>
       <ul>
         <li>
           <span>公司ID </span>
+          <span>年份</span>          
           <span class="center">K4_average</span>
           <span class="center">Median </span>
           <span class="center">average</span>
         </li>
-        <li v-for="(data, key) in dataMap" :key="key">
-          <span>{{key}}</span>
-          <span>{{data.k4_average}}</span>
-          <span>{{data.median}}</span>
-          <span>{{data.average}}</span>
+      </ul>
+      <ul v-show="isShowResult" v-for="(items, k1) in Array.from(dataMap)" :key="k1">
+        <li v-for="(item, k2) in Array.from(items[1])" :key="k2">
+          <span>{{items[0]}}</span>
+          <span>{{item[0]}}</span>
+          <span>{{item[1].k4_average}}</span>
+          <span>{{item[1].median}}</span>
+          <span>{{item[1].average}}</span>
         </li>
       </ul>
     </div>
@@ -23,141 +39,322 @@
 </template>
 
 <script>
+import fs from "fs";
+import path from "path";
 const ml = require("ml-regression");
 const csv = require("csvtojson");
-import fs from 'fs'
-import path from 'path'
+const Json2csvParser = require("json2csv").Parser;
+const { dialog } = require("electron");
 export default {
   name: "landing-page",
   components: {},
   data() {
     return {
-      dataMap: null,
+      isShowResult: true,
+      dataMap: new Map(),
+      companyId: null,
+      year: null,
+      result: null,
+      file: {}
     };
   },
   methods: {
+    action() {
+      this.isShowResult = !this.isShowResult;
+    },
+    clear() {
+      this.dataMap = new Map();
+    },
+    search() {
+      if (this.dataMap.size <= 0) {
+        alert("还没有开始计算，没有数据可供查询，请先计算...");
+        return false;
+      }
+      if (this.companyId == null) {
+        alert("请输入公司...");
+        return false;
+      }
+      if (this.year == null) {
+        alert("请输入年份...");
+        return false;
+      }
+      const companyId = this.companyId.toString();
+      const year = Number(this.year);
+      if (!this.dataMap.has(companyId)) {
+        alert(`没有公司${companyId}的数据`);
+        return false;
+      }
+      if (!this.dataMap.get(companyId).has(year)) {
+        alert(`没有公司${companyId}在${year}年的数据，请查询其它年份`);
+        return false;
+      }
+      this.result = this.dataMap.get(companyId).get(year);
+    },
+    uploadfile(event) {
+      console.log(event.target.files[0]);
+      this.file = event.target.files[0];
+      event.target.value = null;
+    },
+    download() {
+      if (this.dataMap.size > 0) {
+        const fields = ["companyId", "year", "k4_average", "median", "average"];
+        const transformData = [];
+        for (let [companyId, value] of this.dataMap) {
+          for (let [year, v2] of value) {
+            const tmpData = {
+              companyId,
+              year,
+              k4_average: v2.k4_average,
+              median: v2.median,
+              average: v2.average
+            };
+            transformData.push(tmpData);
+          }
+        }
+        const json2csvParser = new Json2csvParser({ fields });
+        const csv = json2csvParser.parse(transformData);
+        console.log(csv);
+        const out = fs.createWriteStream(
+          path.join(__static, "calc/result.csv"),
+          {
+            encoding: "utf8"
+          }
+        );
+        out.write(csv);
+        out.end();
+      } else {
+        alert("请先计算结果!");
+      }
+    },
     calc() {
       const _this = this;
       const SLR = ml.SLR; // Simple Linear Regression
-      const YEAR = 2010; // 观测数据发现 基本都是2007 ~ 2010, 只能计算2010年的可比性，所有数据都要参与计算。
       const companyDataMap = new Map(); // 将数据按照公司归类
       const nindnmeMap = new Map(); // 将公司按照行业归类
-      const coefficientMap = new Map(); // 每个公司的回归系数
-      const SCALE = 100; // 最后数值的缩放比例，取倒数
-      csv({ delimiter: ";" })
-        .fromStream(fs.createReadStream(path.join(__static, 'calc/data.csv')))
+      const coefficientMap = new Map(); // 每个公司,每个年份的回归系数，是个二位数组
+      const relateCompanyMap = new Map(); // 每个年份的同行业公司
+      let comparableMap = new Map(); // 每个年份的同行业公司
+      if (this.dataMap.size > 0) return false;
+      const stream = this.file.path
+        ? fs.createReadStream(this.file.path)
+        : fs.createReadStream(path.join(__static, "calc/data.csv"));
+      csv({ delimiter: "," })
+        .fromStream(stream)
         .then(jsonArray => {
-          // 将数据按公司分类
+          // 将数据按公司和年份分类
           for (let i = 0, len = jsonArray.length; i < len; i++) {
             const json = jsonArray[i];
-            if (companyDataMap.has(json.id)) {
-              companyDataMap.get(json.id).data.push(json);
-            } else {
-              companyDataMap.set(json.id, {
-                nindnme: json.Nindnme,
-                data: [json]
-              });
-            }
-          }
-          console.log("删除小于4年的无效数据:");
-          console.log(`删除前的数据量为${companyDataMap.size}`);
-          // 如果数据没有超过4年，则剔除
-          for (let [key, value] of companyDataMap) {
-            if (value.data.length < 16) {
-              companyDataMap.delete(key);
-            }
-          }
-          console.log(`删除后的数据量为${companyDataMap.size}`);
-          // 先计算出所有组数据的回归系数
-          for (let [key, value] of companyDataMap) {
-            const x = [];
-            const y = [];
-            const reserveArr = [];
-            for (let i = 0, len = value.data.length; i < len; i++) {
-              const row = value.data[i];
-              if (!isNaN(row.earning1) && !isNaN(row.returnSe)) {
-                if (
-                  row.earning1 != 0 &&
-                  row.earning1 != "" &&
-                  row.returnSe != 0 &&
-                  row.returnSe != ""
-                ) {
-                  row.returnSe = f(row.returnSe);
-                  row.earning1 = f(row.earning1) / SCALE;
-                  x.push(row.returnSe);
-                  y.push(row.earning1);
-                  reserveArr.push(row);
+            // 剔除无效数据
+            if (
+              !isNaN(json.earning1) &&
+              !isNaN(json.returnSe) &&
+              !isNaN(json.year)
+            ) {
+              if (
+                json.earning1 != 0 &&
+                json.earning1 != "" &&
+                json.returnSe != 0 &&
+                json.returnSe != "" &&
+                json.year != 0 &&
+                json.year != "" &&
+                json.Nindnme != 0 &&
+                json.Nindnme != ""
+              ) {
+                json.returnSe = f(json.returnSe);
+                json.earning1 = f(json.earning1);
+                json.year = Number(json.year);
+                if (companyDataMap.has(json.id)) {
+                  // 假设数据不会重复
+                  if (companyDataMap.get(json.id).has(json.year)) {
+                    companyDataMap
+                      .get(json.id)
+                      .get(json.year)
+                      .data.push(json);
+                  } else {
+                    companyDataMap
+                      .get(json.id)
+                      .set(json.year, { nindnme: json.Nindnme, data: [json] });
+                  }
+                } else {
+                  // 初始化
+                  companyDataMap.set(
+                    json.id,
+                    new Map([
+                      [json.year, { nindnme: json.Nindnme, data: [json] }]
+                    ])
+                  );
                 }
               }
             }
-            value.data = reserveArr;
-            if (x.length < 2) {
-              companyDataMap.delete(key);
-              continue;
-            }
-            const coefficient = performRegression(x, y);
-            coefficientMap.set(key, coefficient);
           }
-          // 将公司按行业归类
+          // check 是否中途变更行业
+          let count = 0;
           for (let [key, value] of companyDataMap) {
-            const nindnme = value.nindnme;
-            if (nindnmeMap.has(nindnme)) {
-              nindnmeMap.get(nindnme).push(key);
-            } else {
-              nindnmeMap.set(nindnme, [key]);
+            let temp = new Set();
+            for (let v of value.values()) {
+              if (!temp.has(v.nindnme)) {
+                temp.add(v.nindnme);
+              }
+            }
+            if (temp.size > 1) {
+              count++;
             }
           }
-          console.log("删除只有1个公司的行业:");
-          console.log(`删除前的数据量为${nindnmeMap.size}`);
-          for (let [key, value] of nindnmeMap) {
-            if (value.length < 2) {
-              nindnmeMap.delete(key);
-              companyDataMap.delete(value[0]);
+          console.log(`剔除无效数据后仍然多达${count}家`);
+          // 先计算出所有公司所有年份的回归系数
+          for (let [companyId, yearMap] of companyDataMap) {
+            const x = [];
+            const y = [];
+            // 循环判断可以计算回归系数的年份
+            for (let year of yearMap.keys()) {
+              // 计算此年份的前16个月是否存在，存在则可以计算系数
+              let season = 0;
+              for (let i = 0; i < 4; i++) {
+                if (yearMap.has(year - i)) {
+                  if (
+                    yearMap.get(year - i).nindnme == yearMap.get(year).nindnme
+                  ) {
+                    season += yearMap.get(year - i).data.length;
+                  }
+                }
+              }
+              if (season < 16) continue;
+              for (let i = 0; i < 4; i++) {
+                for (
+                  let j = 0, len = yearMap.get(year - i).data.length;
+                  j < len;
+                  j++
+                ) {
+                  const row = yearMap.get(year - i).data[j];
+                  x.push(row.returnSe);
+                  y.push(row.earning1);
+                }
+              }
+              const coefficient = performRegression(x, y);
+              if (coefficientMap.has(companyId)) {
+                coefficientMap
+                  .get(companyId)
+                  .set(year, coefficient.concat([yearMap.get(year).nindnme]));
+              } else {
+                coefficientMap.set(
+                  companyId,
+                  new Map([
+                    [year, coefficient.concat([yearMap.get(year).nindnme])]
+                  ])
+                );
+              }
             }
           }
-          console.log(`删除后的数据量为${nindnmeMap.size}`);
-          // 开始计算2010年的会计可比性
-          // 假设数据都是严格按照年份顺序排列的 2007 ~ 2010， 1~4季度
-          // const _earningArray = calculateAllEarning("2");
-          // console.log(_earningArray);
-          _this.dataMap = {};
-          for (let key of companyDataMap.keys()) {
-            const _earningArray = calculateAllEarning(key);
-            const _k4_average = k4_average(_earningArray);
-            const _median = median(_earningArray);
-            const _average = average(_earningArray);
-            _this.dataMap[key] = {
-              k4_average: _k4_average,
-              median: _median,
-              average: _average
-            };
+          console.log("仍然有多少家公司可以求值：" + coefficientMap.size);
+          // 计算每个年份的同行业公司
+          for (let [companyId, yearMap] of coefficientMap) {
+            for (let [year, coefficient] of yearMap) {
+              if (relateCompanyMap.has(year)) {
+                if (relateCompanyMap.get(year).has(coefficient[2])) {
+                  relateCompanyMap
+                    .get(year)
+                    .get(coefficient[2])
+                    .push(companyId);
+                } else {
+                  relateCompanyMap.get(year).set(coefficient[2], [companyId]);
+                }
+              } else {
+                relateCompanyMap.set(
+                  year,
+                  new Map([[coefficient[2], [companyId]]])
+                );
+              }
+            }
           }
+          for (let [year, nindnmeMap] of relateCompanyMap) {
+            for (let [nindnme, data] of nindnmeMap) {
+              if (data.length < 2) {
+                console.log(`${year}年${nindnme}行业只有一个公司: ${data[0]}`);
+              }
+            }
+          }
+          // 开始计算各公司各年度的会计可比性
+          comparableMap = new Map();
+          for (let [companyId, yearMap] of coefficientMap) {
+            for (let [year, coefficient] of yearMap) {
+              // console.log(`开始计算公司${companyId} ${year}年的会计可比性:`);
+              // 获取公司这一年前16个季度的数据
+              const _thisNindnme = coefficient[2];
+              const _seasonData = get16seasonData(companyId, year);
+              const _thisEarnig = calculateEarning(_seasonData, coefficient);
+              const _relateCompany = relateCompanyMap
+                .get(year)
+                .get(_thisNindnme);
+              if (_relateCompany.length > 1) {
+                const _earningArray = [];
+                for (let i = 0, len = _relateCompany.length; i < len; i++) {
+                  if (_relateCompany[i] == companyId) continue;
+                  const _relateCompanyCoefficient = coefficientMap
+                    .get(_relateCompany[i])
+                    .get(year);
+                  const _earning = calculateEarning(
+                    _seasonData,
+                    _relateCompanyCoefficient
+                  );
+                  _earningArray.push(absoluteDistance(_earning, _thisEarnig));
+                }
+                const _k4_average = k4_average(_earningArray);
+                const _median = median(_earningArray);
+                const _average = average(_earningArray);
+                if (comparableMap.has(companyId)) {
+                  comparableMap.get(companyId).set(year, {
+                    k4_average: _k4_average,
+                    median: _median,
+                    average: _average
+                  });
+                } else {
+                  comparableMap.set(
+                    companyId,
+                    new Map([
+                      [
+                        year,
+                        {
+                          k4_average: _k4_average,
+                          median: _median,
+                          average: _average
+                        }
+                      ]
+                    ])
+                  );
+                }
+                _this.dataMap = comparableMap;
+                console.log(
+                  `公司${companyId} ${year}年的会计可比性:${_k4_average}`
+                );
+              } else {
+                console.log(
+                  `${year}年${_thisNindnme}行业不超过两家公司: ${_relateCompany}, 不具备行业可比性`
+                );
+              }
+            }
+          }
+          // calculating end. test
+          console.log(_this.dataMap.get("2").get(2010));
         });
-
       function performRegression(x, y) {
         const regressionModel = new SLR(x, y); // Train the model on training data
         // console.log(regressionModel.coefficients[1])
         return regressionModel.coefficients;
       }
 
-      function calculateAllEarning(id) {
-        const earningArray = [];
-        const _thisCompanyData = companyDataMap.get(id).data;
-        const _thisNindnme = companyDataMap.get(id).nindnme;
-        const companies = nindnmeMap.get(_thisNindnme);
-        const _thisEarnig = calculateEarning(_thisCompanyData, id);
-        for (let i = 0, len = companies.length; i < len; i++) {
-          if (companies[i] == id) continue;
-          const _earning = calculateEarning(_thisCompanyData, companies[i]);
-          earningArray.push(absoluteDistance(_earning, _thisEarnig));
+      function get16seasonData(id, year) {
+        let seasonData = [];
+        for (let i = 0; i < 4; i++) {
+          seasonData = seasonData.concat(
+            companyDataMap.get(id).get(year - i).data
+          );
         }
-        return earningArray;
+        return seasonData;
       }
 
       // data: 公司数据 id: 需要计算的公司ID
-      function calculateEarning(data, id) {
+      function calculateEarning(data, coefficient) {
         const earning = [];
-        const coefficient = coefficientMap.get(id);
         const intercept = coefficient[0]; // y = slope * x + intercept;
         const slope = coefficient[1];
         for (let i = 0, len = data.length; i < len; i++) {
@@ -210,7 +407,7 @@ export default {
 </script>
 
 <style>
-/* @import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro"); */
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
 
 * {
   box-sizing: border-box;
@@ -261,7 +458,18 @@ body {
   color: black;
   margin-bottom: 10px;
 }
-
+.doc .tips {
+  color: red;
+}
+.doc input {
+  font-size: 0.8em;
+  width: 10em;
+  padding: 0.75em 2em;
+  margin-right: 2em;
+  border: 1px solid #4fc08d;
+  border-radius: 2em;
+  box-sizing: border-box;
+}
 .doc button {
   font-size: 0.8em;
   cursor: pointer;
